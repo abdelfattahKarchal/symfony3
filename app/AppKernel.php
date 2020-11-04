ee0dba7cd343f0f1d08eee1eb53b82e765ef621e
abdelfattah <abdelfattah@MacBook-Pro-de-abdelfattah.local>
2020-11-04 15:46:10 +0100

    Routage avec YAML et validation des paramettres

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new SiteBundle\SiteBundle(),
             new TestBundle\TestBundle(),
             new BlogBundle\BlogBundle(),
+            new StockBundle\StockBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
